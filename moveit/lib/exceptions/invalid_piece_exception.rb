ce44c7378d1af52ffe31aa4a5728882ce0339d63
John F Schank III <jschank@mac.com>
2014-03-04 18:10:36 -0500

    removed unused exception

--- moveit/lib/exceptions/invalid_piece_exception.rb
+++
@@ -1,6 +0,0 @@
-# Exception raised when a attempt is made to create an invalid chess piece
-class InvalidPieceException < StandardError
-  def initialize(args)
-    super("Invalid piece exception #{args}")
-  end
-end
