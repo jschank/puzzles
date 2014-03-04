7d208052603e181402162892f2d70e0d346ae737
John F Schank III <jschank@mac.com>
2014-03-04 17:50:48 -0500

    Queen works now

--- moveit/lib/moveit/queen.rb
+++ moveit/lib/moveit/queen.rb
@@ -6,5 +6,14 @@ module MoveIt
     def show
       color_code + 'Q'
     end
+
+    def valid_move?(start, target, board)
+      (Chessboard.same_rank?(start, target) ||
+       Chessboard.same_file?(start, target) ||
+       Chessboard.same_diagonal?(start, target)) &&
+      board.path_clear?(start, target) &&
+      (!board.occupied?(target) || self.is_opponent?(board.piece_at(target))) &&
+      king_safe(start, target, board)
+    end
   end
 end
--- moveit/test/test_queen.rb
+++ moveit/test/test_queen.rb
@@ -9,4 +9,86 @@ class TestQueen < MiniTest::Test
     piece = Chesspiece.create('wQ')
     assert_equal('wQ', piece.show)
   end
+  def test_movement
+    board = Chessboard.new
+    piece = Queen.new(:white)
+    board.add(piece, 'd4')
+
+    assert(piece.valid_move?('d4', 'g4', board), 'Queen can move on rank')
+    assert(piece.valid_move?('d4', 'a4', board), 'Queen can move on rank')
+    assert(piece.valid_move?('d4', 'd1', board), 'Queen can move on file')
+    assert(piece.valid_move?('d4', 'd5', board), 'Queen can move on file')
+
+    assert(piece.valid_move?('d4', 'f6', board), 'Queen can move on diagonal')
+    assert(piece.valid_move?('d4', 'c5', board), 'Queen can move on diagonal')
+    assert(piece.valid_move?('d4', 'g1', board), 'Queen can move on diagonal')
+    assert(piece.valid_move?('d4', 'b2', board), 'Queen can move on diagonal')
+
+    refute(piece.valid_move?('d4', 'b3', board), 'Queen can not move like a knight')
+    refute(piece.valid_move?('d4', 'a3', board), 'Queen can not move anywhere other than rank or file or diagonal')
+  end
+
+  def test_blockers
+    board = Chessboard.new
+    piece = Queen.new(:black)
+    board.add(piece, 'd4')
+
+    board.add(Pawn.new(:black), 'e4')
+    board.add(Pawn.new(:white), 'd2')
+    board.add(Pawn.new(:black), 'e5')
+    board.add(Pawn.new(:white), 'b2')
+
+    refute(piece.valid_move?('d4', 'h4', board), 'Queen blocked by friendly piece')
+    refute(piece.valid_move?('d4', 'd1', board), 'Queen blocked by opponent')
+    refute(piece.valid_move?('d4', 'f6', board), 'Queen blocked by friendly piece')
+    refute(piece.valid_move?('d4', 'a1', board), 'Queen blocked by opponent')
+  end
+
+  def test_capture
+    board = Chessboard.new
+    piece = Queen.new(:black)
+    board.add(piece, 'd4')
+
+    # rook style captures
+    board.add(Pawn.new(:black), 'e4')
+    board.add(Pawn.new(:white), 'b4')
+    board.add(Pawn.new(:white), 'd7')
+
+    # bishop style captures
+    board.add(Pawn.new(:black), 'e5')
+    board.add(Pawn.new(:white), 'b2')
+    board.add(Pawn.new(:white), 'a7')
+
+    assert(piece.valid_move?('d4', 'b4', board), 'Queen can capture opponent')
+    assert(piece.valid_move?('d4', 'd7', board), 'Queen can capture opponent')
+    assert(piece.valid_move?('d4', 'b2', board), 'Queen can capture opponent')
+    assert(piece.valid_move?('d4', 'a7', board), 'Queen can capture opponent')
+
+    refute(piece.valid_move?('d4', 'e4', board), 'Queen cannot capture friendly piece')
+    refute(piece.valid_move?('d4', 'e5', board), 'Queen cannot capture friendly piece')
+  end
+
+  def test_exposed_check_rook_style
+    board = Chessboard.new
+    piece = Queen.new(:white)
+    board.add(piece, 'd4')
+
+    board.add(King.new(:white), 'd2')
+    board.add(Queen.new(:black), 'd8')
+
+    refute(piece.valid_move?('d4', 'b4', board), 'Queen cannot move to expose king to check')
+    assert(piece.valid_move?('d4', 'd8', board), 'Queen can capture threatening piece')
+  end
+
+  def test_exposed_check_bishop_style
+    board = Chessboard.new
+    piece = Queen.new(:white)
+    board.add(piece, 'd4')
+
+    board.add(King.new(:white), 'b2')
+    board.add(Queen.new(:black), 'f6')
+
+    refute(piece.valid_move?('d4', 'b6', board), 'Queen cannot move to expose king to check')
+    assert(piece.valid_move?('d4', 'f6', board), 'Queen can capture threatening piece')
+  end
 end
