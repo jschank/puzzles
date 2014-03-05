0f97e78fd3080df09cc432eeb0ab502735ee1c8e
John F Schank III <jschank@mac.com>
2014-03-05 03:17:33 -0500

    added some new resources after verifying, and created some regression tests for cases that are incorrectly evaluated

---
+++ moveit/test/test_regressions.rb
@@ -0,0 +1,23 @@
+require 'minitest_helper'
+
+# test chessboard functionality
+class TestRegressions < MiniTest::Test
+  def setup
+    filepath = Pathname.new("resources").join('complex_board.txt')
+    @board = Chessboard.new
+    @board.load(filepath)
+  end
+
+  def test_complex_board_c3_a2_should_be_legal
+    assert(@board.valid_move?('c3', 'a2'), "Complex Board c3 to a2 should be legal")
+  end
+
+  def test_complex_board_c5_c4_should_be_legal
+    assert(@board.valid_move?('c5', 'c4'), "Complex Board c5 to c4 should be legal")
+  end
+
+  def test_complex_board_e6_g4_should_be_legal
+    assert(@board.valid_move?('e6', 'g4'), "Complex Board e6 to g4 should be legal")
+  end
+
+end
