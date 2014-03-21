cc91897963a1bb163b4fcc89f621416fb4fafa4c
John F Schank III <jschank@mac.com>
2014-03-20 20:50:58 -0400

    Renamed and cleaned up a little

---
+++ music-yaml-parsing/yaml_data.rb
@@ -0,0 +1,49 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'yaml'
+
+class YamlData
+  def initialize(data)
+    @data = data
+  end
+
+  def method_missing(sym, *args, &block)
+    key = sym.to_s
+    if @data.keys.include?(key)
+      value = @data[key]
+      case value
+      when Hash
+        YamlData.new(value)
+      when Array
+        value.map { |item| YamlData.new(item) }
+      else
+        value
+      end
+    else
+      super
+    end
+  end
+end
+
+
+class TestYamlData < MiniTest::Test
+
+  def setup
+    @yaml = YAML.load_file('music.yaml')
+  end
+
+  def test_yaml_load_is_working
+    data = @yaml
+    assert_equal("But Not For Me", data["genres"].last["artists"].first["albums"].first["tracks"].last["name"])
+  end
+
+  def test_YamlData_interprets_hash_keys_as_method_calls
+    data = YamlData.new(@yaml)
+    assert_equal("But Not For Me", data.genres.last.artists.first.albums.first.tracks.last.name)
+  end
+
+  def test_yaml_load_is_working_2
+    raise @yaml.inspect
+  end
+end
+
