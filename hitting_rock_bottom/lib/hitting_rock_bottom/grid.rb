f36aa6dab5ddfa7ba9b7a66691e27b1b3d464053
John F Schank III <jschank@mac.com>
2014-03-31 16:06:27 -0400

    two dimensional empty grid working

--- hitting_rock_bottom/lib/hitting_rock_bottom/grid.rb
+++ hitting_rock_bottom/lib/hitting_rock_bottom/grid.rb
@@ -1,19 +1,22 @@
 module HittingRockBottom
   class Grid
+    def initialize(rows = 0, cols = 0)
+      @grid = Array.new(rows) { Array.new(cols) }
+    end
     def empty?
-      true
+      @grid.all? { |col| col.empty? }
     end
 
     def rows
-      []
+      @grid
     end
 
     def columns
-      []
+      @grid.transpose
     end
 
     def cells
-      0
+      empty? ? 0 : @grid.length * @grid[0].length
     end
   end
 end
--- hitting_rock_bottom/test/hitting_rock_bottom/grid_test.rb
+++ hitting_rock_bottom/test/hitting_rock_bottom/grid_test.rb
@@ -3,9 +3,18 @@ require 'minitest_helper'
 class GridTest < MiniTest::Test
   def test_it_can_create_an_empty_two_dimensional_grid
     g = Grid.new()
-    assert(g.empty?)
-    assert_equal([], g.rows)
-    assert_equal([], g.columns)
-    assert_equal(0, g.cells)
+    assert(g.empty?, 'New Grid is empty')
+    assert_equal([], g.rows, 'New Grid has no rows')
+    assert_equal([], g.columns, 'New Grid has no columns')
+    assert_equal(0, g.cells, 'New Grid has no cells')
   end
+
+  def test_it_can_create_empty_2_by_3_grid
+    g = Grid.new(2, 3)
+    refute(g.empty?, 'inital grid is not empty')
+    assert_equal([[nil, nil, nil], [nil, nil, nil]], g.rows, 'Initial grid has rows filled with nil')
+    assert_equal([[nil, nil], [nil, nil], [nil, nil]], g.columns, 'Initial grid has columns filled will nil')
+    assert_equal(6, g.cells, 'Inital grid has correct number of cells')
+  end
+
 end
