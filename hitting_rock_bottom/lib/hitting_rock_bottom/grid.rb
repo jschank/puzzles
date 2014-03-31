79219d035bd228aefbc2050bc4ed5026d7f8bec4
John F Schank III <jschank@mac.com>
2014-03-31 16:32:49 -0400

    Added bounds checking

--- hitting_rock_bottom/lib/hitting_rock_bottom/grid.rb
+++ hitting_rock_bottom/lib/hitting_rock_bottom/grid.rb
@@ -1,7 +1,9 @@
 module HittingRockBottom
   class Grid
-    def initialize(rows = 0, cols = 0)
-      @grid = Array.new(rows) { Array.new(cols) }
+    def initialize(rows = 0, columns = 0)
+      @rows = rows
+      @columns = columns
+      @grid = Array.new(@rows) { Array.new(@columns) }
     end
     def empty?
       @grid.all? { |col| col.empty? }
@@ -16,7 +18,24 @@ module HittingRockBottom
     end
 
     def cells
-      empty? ? 0 : @grid.length * @grid[0].length
+      empty? ? 0 : @rows * @columns
     end
+
+    def [](row, column)
+      bounds_check(row, column)
+      @grid[row][column]
+    end
+
+    def []=(row, column, value)
+      bounds_check(row, column)
+      @grid[row][column] = value
+    end
+
+    private
+    def bounds_check(row, column)
+      fail(IndexError, "Row #{row} is out of range for this grid", caller) if row >= @rows
+      fail(IndexError, "Column #{column} is out of range for this grid", caller) if column >= @columns
+    end
+
   end
 end
--- hitting_rock_bottom/test/hitting_rock_bottom/grid_test.rb
+++ hitting_rock_bottom/test/hitting_rock_bottom/grid_test.rb
@@ -17,4 +17,12 @@ class GridTest < MiniTest::Test
     assert_equal(6, g.cells, 'Inital grid has correct number of cells')
   end
 
+  def test_it_can_get_and_set_cell_values
+    g = Grid.new(1, 1)
+    g[0,0] = 'x'
+    assert_equal('x', g[0,0], 'Grid allows user to retrieve value of a cell')
+    assert_raises(IndexError) {g[2,0]}
+    assert_raises(IndexError) {g[0,2]}
+  end
+
 end
