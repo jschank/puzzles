bfe660819916c87912ee8c617bb836a90b78da6c
John F Schank III <jschank@mac.com>
2014-03-31 15:06:31 -0400

    Restructured test directory to match lib directory

--- hitting_rock_bottom/test/hitting_rock_bottom/version_test.rb
+++ hitting_rock_bottom/test/hitting_rock_bottom/version_test.rb
@@ -1,8 +1,7 @@
 require 'minitest_helper'
 
-class TestHittingRockBottom < MiniTest::Unit::TestCase
+class TestHittingRockBottom < MiniTest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HittingRockBottom::VERSION
   end
-
 end
