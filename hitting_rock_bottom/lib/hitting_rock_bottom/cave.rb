349368fc20127b8fe06d51eff7481523d004f28d
John F Schank III <jschank@mac.com>
2014-04-01 16:31:30 -0400

    added depth report to cave

--- hitting_rock_bottom/lib/hitting_rock_bottom/cave.rb
+++ hitting_rock_bottom/lib/hitting_rock_bottom/cave.rb
@@ -4,17 +4,41 @@ module HittingRockBottom
     Water = '~'
     Rock = '#'
 
-    def initialize(height = 0, length = 0, fill = nil)
+    def initialize(string_array = [""])
+      height = string_array.length
+      length = string_array[0].length
       @cave = Grid.new(height, length)
       height.times do |height|
         length.times do |length|
-          @cave[height, length] = fill
+          @cave[height, length] = string_array[height][length]
         end
       end
+      influx = find_influx
+      @water_head = influx
+    end
+
+    def add_water
+      # try below
+      # try to the right
+      # go to leftmost Air one row up
+    end
+
+    def depth_report
+      @cave.columns.map{|c| c.select{ |i| i == Water}.length }.join(' ')
     end
 
     def to_s
       @cave.to_s
     end
+
+    private
+    def find_influx
+      # Assumption - Influx is always in column 0.
+      return nil if @cave.empty?
+      col = @cave.columns[0]
+      row = col.index(Water)
+      [row,0]
+    end
+
   end
 end
--- hitting_rock_bottom/test/hitting_rock_bottom/cave_test.rb
+++ hitting_rock_bottom/test/hitting_rock_bottom/cave_test.rb
@@ -6,29 +6,125 @@ class CaveTest < MiniTest::Test
     assert_equal("", c.to_s, "Can create an empty cave")
   end
 
-  def test_that_it_can_create_an_airy_cave
-    c = Cave.new(2,2,Cave::Air)
-    assert_equal("  \n  ", c.to_s, "Can create a cave full of air")
+  def test_that_it_can_create_a_cave_from_array_of_strings
+    cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+    #####
+    ~   #
+    # # #
+    #####
+    EOS
+    c = Cave.new(cave_string.split("\n"))
+    assert_equal(cave_string, c.to_s, 'Cave can be loaded from an array of strings')
   end
 
-  def test_that_it_can_create_a_rocky_cave
-    c = Cave.new(3,3,Cave::Rock)
-    cave_str = <<-'EOS'.gsub(/^\s+/, '').chomp
-    ###
-    ###
-    ###
+  def test_that_it_can_report_water_depths
+    cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+    ################################
+    ~~~~~~~~~~~~~~~                #
+    #~~~~~~~~~####~~~~~~~~~~~~     #
+    ###~~~~~~~####~~~~~~~~~~~~~~~~##
+    ###~~~~~~~####~~~~~~~~~~~~~~####
+    #######~~~#######~~~~~~~~~######
+    #######~~~###########~~~~~######
+    ################################
     EOS
-    assert_equal(cave_str, c.to_s, "Can create a cave full of rock")
+    expected_depth_report = '1 2 2 4 4 4 4 6 6 6 1 1 1 1 4 3 3 4 4 4 4 5 5 5 5 5 2 2 1 1 0 0'
+    c = Cave.new(cave_string.split("\n"))
+    assert_equal(expected_depth_report, c.depth_report, 'Cave can report water depths')
   end
 
-  def test_that_it_can_create_a_watery_cave
-    c = Cave.new(4,5,Cave::Water)
-    cave_str = <<-'EOS'.gsub(/^\s+/, '').chomp
-    ~~~~~
-    ~~~~~
-    ~~~~~
-    ~~~~~
-    EOS
-    assert_equal(cave_str, c.to_s, "Can create a cave full of water")
-  end
+  # def test_that_it_can_add_water_to_right
+  #   cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~     #
+  #   #     #
+  #   #     #
+  #   #  #  #
+  #   #######
+  #   EOS
+  #   c = Cave.new(cave_string.split("\n"))
+  #
+  #   c.add_water
+  #   cave_string_after_water_added = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #     #
+  #   #     #
+  #   #  #  #
+  #   #######
+  #   EOS
+  #   assert_equal(cave_string_after_water_added, c.to_s, 'Cave is correct after 1 unit of water added')
+  # end
+
+  # def test_that_it_can_add_water_down
+  #   cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #     #
+  #   #     #
+  #   #  #  #
+  #   #######
+  #   EOS
+  #   c = Cave.new
+  #   c.load(cave_string.split("\n"))
+  #
+  #   c.add_water
+  #   cave_string_after_water_added = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #~    #
+  #   #     #
+  #   #  #  #
+  #   #######
+  #   EOS
+  #   assert_equal(cave_string_after_water_added, c.to_s, 'Cave is correct after 1 unit of water added')
+  # end
+  #
+  # def test_that_it_can_add_water_at_bottom
+  #   cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #~    #
+  #   #~    #
+  #   #~ #  #
+  #   #######
+  #   EOS
+  #   c = Cave.new
+  #   c.load(cave_string.split("\n"))
+  #
+  #   c.add_water
+  #   cave_string_after_water_added = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #~    #
+  #   #~    #
+  #   #~~#  #
+  #   #######
+  #   EOS
+  #   assert_equal(cave_string_after_water_added, c.to_s, 'Cave is correct after 1 unit of water added')
+  # end
+  #
+  # def test_that_adding_water_raises_level
+  #   cave_string = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #~    #
+  #   #~    #
+  #   #~~#  #
+  #   #######
+  #   EOS
+  #   c = Cave.new
+  #   c.load(cave_string.split("\n"))
+  #
+  #   c.add_water
+  #   cave_string_after_water_added = <<-'EOS'.gsub(/^\s+/, '').chomp
+  #   #######
+  #   ~~    #
+  #   #~    #
+  #   #~~   #
+  #   #~~#  #
+  #   #######
+  #   EOS
+  #   assert_equal(cave_string_after_water_added, c.to_s, 'Cave is correct after 1 unit of water added')
+  # end
 end
