1068f24e45c408702fc1a2eb2526ee40ac4d5e4f
John F Schank III <jschank@mac.com>
2014-04-01 18:08:53 -0400

    Getting binary to work

--- hitting_rock_bottom/lib/hitting_rock_bottom.rb
+++ hitting_rock_bottom/lib/hitting_rock_bottom.rb
@@ -1,4 +1,6 @@
-require "hitting_rock_bottom/version"
+require 'hitting_rock_bottom/version'
+require 'hitting_rock_bottom/cave'
+require 'hitting_rock_bottom/grid'
 
 module HittingRockBottom
   # Your code goes here...
