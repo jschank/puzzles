b193d468b60c723a3472ab689c95d07a85498845
John F Schank III <jschank@mac.com>
2015-03-01 11:56:58 -0500

    Added another method for finding missing value in a sequence

--- scratch/missing_value.rb
+++ scratch/missing_value.rb
@@ -14,3 +14,10 @@ missing_number = array.inject(xor) { |acc, n| acc = acc ^ n }
 
 puts "Number removed = #{missing_value}"
 puts "Calculated missing value = #{missing_number}"
+
+# my method...
+total = N * (N+1) / 2
+sum = array.inject(&:+)
+missing_number = total - sum
+
+puts "MY Calculated missing value = #{missing_number}"
